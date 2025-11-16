30f8968588767248a87ab365adffb02023dd8117
yuanji_mac <yuanji_hust@qq.com>
2025-11-16 14:32:08 +0800

    完成13.2.1行为像值的类

---
+++ Chapter13_Copyctrl/Hasptr_value.h
@@ -0,0 +1,29 @@
+//行为像值的类
+#include <iostream>
+#include <string>
+using namespace std;
+class HasPtr {
+public:
+    /*如果不声明这样的拷贝函数和运算符，进行的是浅拷贝，直接把指针赋值过去*/
+    //copy constructor
+    HasPtr(const HasPtr & x){ps=new string(*x.ps); i=x.i;}
+    HasPtr(const string &s =string()):ps(new string(s)),i(0){}
+    //copy operator
+    HasPtr& operator=(const HasPtr& x){ 
+        /*
+        编写赋值运算符的原则：对象赋予他自身时必须能工作
+        */
+        auto newp=new string(*x.ps);//先存入局部临时对象
+        delete ps;//释放旧内存
+        ps=newp;
+        i=x.i;
+        return *this;    
+    }
+    //deconstructor
+    ~HasPtr(){delete ps;}
+    void print(){cout<<*ps<<" "<<i<<endl;}
+    string * returns(){return ps;};
+public:
+    string *ps;
+    int i;
+};
---
+++ Chapter13_Copyctrl/main.cpp
@@ -0,0 +1,22 @@
+#include "Hasptr_value.h"
+int main(){
+    HasPtr a("hello");
+    HasPtr b(a);
+    HasPtr c("test");
+    c=a;
+    a.print();
+    *a.ps="world";
+    a.print();
+    b.print(); 
+    c.print();
+    string *s=nullptr;
+    int i=1;
+    {
+        HasPtr d(a);
+        s=d.returns();
+        i--;
+    }
+    if(s) cout<<"value of pointer d is"<<*s<<endl;
+    else cout<<"d is a nullptr"<<endl;
+    return 0;
+}
